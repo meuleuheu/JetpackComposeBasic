953e6624b7c21e87771e3c8cae2ea68fc6dd634f
meuleuheu <htrung.ng99@gmail.com>
2025-03-22 12:20:12 +0700

    Scroll trong Row, Column

--- app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,12 +20,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -163,6 +173,44 @@ fun Greeting3(name: String, modifier: Modifier) {
 }
 // No.3 end
 
+// No.4
+@Composable
+fun Greeting4_1(name: String, modifier: Modifier) {
+    Column(Modifier.verticalScroll(rememberScrollState())) {
+        Row(Modifier.horizontalScroll(rememberScrollState())) {
+            repeat(10) {
+                Box(Modifier.size(100.dp).padding(10.dp).background(Color.Green))
+            }
+        }
+
+        repeat(10) {
+            Box(Modifier.size(100.dp).padding(10.dp).background(Color.Green))
+        }
+
+        Row(Modifier.horizontalScroll(rememberScrollState())) {
+            repeat(10) {
+                Box(Modifier.size(100.dp).padding(10.dp).background(Color.Green))
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting4_2(name: String, modifier: Modifier) {
+    var offset by remember { mutableStateOf(0f) }
+    Column (
+        Modifier.scrollable(
+            orientation = Orientation.Vertical,
+            state = rememberScrollableState { delta ->
+                offset += delta
+                delta
+            }
+        )){
+        Text("$offset")
+    }
+}
+// No.4 end
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -171,7 +219,7 @@ fun GreetingPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Greeting3("Android", modifier = Modifier)
+            Greeting4_1("Android", modifier = Modifier)
         }
     }
 }
